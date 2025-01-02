bc16c2537ea6bbf71adf61e885e9c7919ef8cebb
nishaaa25 <sahnisha2505@gmail.com>
2025-01-02 20:02:20 +0530

    testing

--- app/meals/not-found.js
+++ app/meals/not-found.js
@@ -4,7 +4,7 @@ export default function NotFound() {
       <div className="p-10 rounded-md border border-red-400">
         <h2 className="custom-heading-text uppercase">Meal Not Found</h2>
         <p className="custom-para-text">
-          Unfortunately, we could not find the requested meal data.
+          Unfortunately, we could not find hhh the requested meal data.
         </p>
       </div>
     </div>
